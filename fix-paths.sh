0f159287a8a8b4ce7268cb75079cc85f3a75e36e
imwm <mwmoretti2@gmail.com>
2025-03-15 13:21:54 -0700

    Revert "Fix paths for Vercel deployment and add Vercel configuration"
    This reverts commit 3c91418690253b98f4c03c9b4dd591e0651a517e.

--- fix-paths.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fix href="public/ references
-find . -name "*.html" -type f -exec sed -i '' 's/href="public\//href="\//' {} \;
-
-# Fix src="public/ references
-find . -name "*.html" -type f -exec sed -i '' 's/src="public\//src="\//' {} \;
-
-echo "All path references to the public directory have been fixed." 
